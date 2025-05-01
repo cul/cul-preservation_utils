001848424775048f8d93896513bf038b1c960ee6
goldsmithb <bg2918@columbia.edu>
2025-05-01 15:22:16 -0400

    initial commit

---
+++ spec/cul/preservation_utils_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Cul::PreservationUtils do
+  it "has a version number" do
+    expect(Cul::PreservationUtils::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
